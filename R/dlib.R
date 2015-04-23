4bb945394c13452e7939bd2bb590445082f20847
Oliver Keyes <ironholds@gmail.com>
2015-04-23 14:06:39 -0400

    start

---
+++ R/dlib.R
@@ -0,0 +1,10 @@
+#' @title R integration with dlib
+#' @name dlib
+#' @description This package makes the dlib C++ library - which contains both generic tools for
+#'              software development, and a BSL-licensed machine learning library - accessible
+#'              from R, so that it can be conveniently included in other packages.
+#' @useDynLib dlib
+#' @docType package
+#' @aliases dlib dlib-package
+#' @importFrom Rcpp sourceCpp
+NULL
