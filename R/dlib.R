54212a5f4df0a8949c1620c39d1dd83bcabd4f46
Oliver Keyes <ironholds@gmail.com>
2015-04-24 15:03:20 -0400

    CRAN

--- R/dlib.R
+++ R/dlib.R
@@ -1,7 +1,7 @@
 #' @title R integration with dlib
 #' @name dlib
 #' @description This package makes the dlib C++ library - which contains both generic tools for
-#'              software development, and a BSL-licensed machine learning library - accessible
+#'              software development, and BSL-licensed machine learning functionality - accessible
 #'              from R, so that it can be conveniently included in other packages.
 #' @useDynLib dlib
 #' @docType package
